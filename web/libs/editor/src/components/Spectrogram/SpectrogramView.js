3b7a14f8d789c49d99a6b9daaf8b8688ed94fd9c
Lukas4144 <schuler.lukas@intergga.ch>
2024-05-02 16:46:59 +0200

    add "labeling function" to PoC

--- web/libs/editor/src/components/Spectrogram/SpectrogramView.js
+++ web/libs/editor/src/components/Spectrogram/SpectrogramView.js
@@ -10,13 +10,13 @@ export default class SpectrogramView extends Component {
     this.state = {
       playbackSpeed: 1.0,
       zoomLevel: 1000,
-      spec_plugin: null
+      spec_plugin: null,
+      isDrawing: false // New state to track drawing status
     };
     this.wsRef = React.createRef();
   }
 
   componentDidMount() {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
     const colormap = require('colormap');
     const colors = colormap({
       colormap: 'hot',
@@ -37,7 +37,6 @@ export default class SpectrogramView extends Component {
       fftSamples: 1024,
     });
 
-
     this.wavesurfer = WaveSurfer.create({
       container: this.wsRef.current,
       waveColor: 'rgb(255, 255, 255)',
@@ -50,16 +49,61 @@ export default class SpectrogramView extends Component {
         this.spec_plugin
       ],
     });
+
     this.wavesurfer.on('ready', () => {
       this.wavesurfer.zoom(this.state.zoomLevel);
     });
+
+    // Add event listeners for drawing
+    const canvas = this.spec_plugin.canvas;
+    canvas.addEventListener('mousedown', this.startDrawing);
+    canvas.addEventListener('mouseup', this.draw);
+    canvas.addEventListener('mouseleave', this.finishDrawing);
   }
+
+  componentWillUnmount() {
+    // Remove event listeners to prevent memory leaks
+    const canvas = this.spec_plugin.canvas;
+    canvas.removeEventListener('mousedown', this.startDrawing);
+    canvas.removeEventListener('mouseup', this.draw);
+    canvas.removeEventListener('mouseleave', this.finishDrawing);
+  }
+
+  startDrawing = (event) => {
+    const canvas = this.spec_plugin.canvas;
+    const boundingRect = canvas.getBoundingClientRect();
+    this.startX = event.clientX - boundingRect.left;
+    this.startY = event.clientY - boundingRect.top;
+
+    this.setState({ isDrawing: true });
+  };
+  draw = (event) => {
+    if (!this.state.isDrawing) return;
+
+    const canvas = this.spec_plugin.canvas;
+    const ctx = canvas.getContext('2d');
+    const boundingRect = canvas.getBoundingClientRect();
+    const x = event.clientX - boundingRect.left;
+    const y = event.clientY - boundingRect.top;
+
+    ctx.fillStyle = 'rgba(255, 255, 255, 0)';
+    ctx.strokeStyle = 'white';
+
+    ctx.beginPath();
+    ctx.rect(this.startX, this.startY, x - this.startX, y - this.startY);
+    ctx.fill();
+    ctx.stroke();
+  };
+
+  finishDrawing = () => {
+    this.setState({ isDrawing: false });
+  };
 
   handleSpeedChange = (event) => {
     const newSpeed = parseFloat(event.target.value);
     this.setState({ playbackSpeed: newSpeed });
     if (this.wavesurfer) {
-      this.wavesurfer.setPlaybackRate(newSpeed,false);
+      this.wavesurfer.setPlaybackRate(newSpeed, false);
     }
   };
 
@@ -82,40 +126,41 @@ export default class SpectrogramView extends Component {
   render() {
     const { playbackSpeed, zoomLevel } = this.state;
     return (
-    <div>
-      <div id="wave" ref={this.wsRef}/>
-      <h1>Controls</h1>
-      <div style={{display: 'flex'}}>
-        <div>
+      <div>
+        <div id="wave" ref={this.wsRef} />
+        <h1>Controls</h1>
+        <div style={{ display: 'flex' }}>
           <div>
             <div>
-              <span>Playback Speed</span>
+              <div>
+                <span>Playback Speed</span>
+              </div>
+              <span>{playbackSpeed}x</span>
             </div>
-            <span>{playbackSpeed}x</span>
+            <input
+              type="range"
+              min="0.1"
+              max="1.0"
+              step="0.1"
+              value={playbackSpeed}
+              onChange={this.handleSpeedChange}
+              onInput={this.handleSpeedChange}
+            />
           </div>
-          <input
-            type="range"
-            min="0.1"
-            max="1.0"
-            step="0.1"
-            value={playbackSpeed}
-            onChange={this.handleSpeedChange}
-            onInput={this.handleSpeedChange}
-          />
-        </div>
 
-        <div style={{marginLeft: '200px'}}>
-          <div>
-            <span>Zoom</span>
-          </div>
-          <span>{zoomLevel}%</span>
-          <div>
-            <button onClick={this.handleZoomOut} style={{marginRight: '5px'}}>-</button>
-            <button onClick={this.handleZoomIn}>+</button>
+          <div style={{ marginLeft: '200px' }}>
+            <div>
+              <span>Zoom</span>
+            </div>
+            <span>{zoomLevel}%</span>
+            <div>
+              <button onClick={this.handleZoomOut} style={{ marginRight: '5px' }}>-</button>
+              <button onClick={this.handleZoomIn}>+</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
   }
 }
+
